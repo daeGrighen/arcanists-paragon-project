3ee8b7df5e60158fb12926901750cec695fab98c
Polpo <endergrigo@gmail.com>
2024-03-09 19:47:14 +0100

    Ritual crafting optimized, particles , still to animate

--- src/main/java/net/polpo/arcanistsparagon/block/custom/RitualCoreBlock.java
+++ src/main/java/net/polpo/arcanistsparagon/block/custom/RitualCoreBlock.java
@@ -1,15 +1,18 @@
 package net.polpo.arcanistsparagon.block.custom;
 
 import com.mojang.serialization.MapCodec;
+import net.fabricmc.loader.impl.lib.sat4j.core.Vec;
 import net.minecraft.block.*;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityTicker;
 import net.minecraft.block.entity.BlockEntityType;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.Inventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.particle.BlockStateParticleEffect;
 import net.minecraft.particle.ParticleTypes;
 import net.minecraft.server.command.ParticleCommand;
 import net.minecraft.sound.SoundCategory;
@@ -19,6 +22,7 @@ import net.minecraft.util.Hand;
 import net.minecraft.util.function.BooleanBiFunction;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
@@ -126,22 +130,25 @@ public class RitualCoreBlock extends BlockWithEntity implements GeoAnimatable{
                 if (!recipeResult.isEmpty()) {
 
                     int maxDrops = getMaxCraft(itemList);
-                    northPedestal.removeStack(0, maxDrops);
-                    southPedestal.removeStack(0, maxDrops);
-                    eastPedestal.removeStack(0, maxDrops);
-                    westPedestal.removeStack(0, maxDrops);
 
+                    northPedestal.removeStack(0, maxDrops);
                     northPedestal.markDirty();
+
+                    southPedestal.removeStack(0, maxDrops);
                     southPedestal.markDirty();
+
+                    eastPedestal.removeStack(0, maxDrops);
                     eastPedestal.markDirty();
+
+                    westPedestal.removeStack(0, maxDrops);
                     westPedestal.markDirty();
 
 
                     double x = pos.getX();
-                    double y = pos.getY() + 1;
+                    double y = pos.getY();
                     double z = pos.getZ();
 
-                    ItemEntity itemResEntity = new ItemEntity(world, x, y, z, recipeResult.copyWithCount(maxDrops));
+                    ItemEntity itemResEntity = new ItemEntity(world, x, y+0.5, z, recipeResult.copyWithCount(maxDrops));
 
                     ArcanistsParagon.LOGGER.info("3");
                     world.spawnEntity(itemResEntity);
@@ -149,6 +156,17 @@ public class RitualCoreBlock extends BlockWithEntity implements GeoAnimatable{
                             SoundEvents.ENTITY_PARROT_IMITATE_WARDEN,
                             SoundCategory.BLOCKS, 2f, 15f);
 
+                    // NO! BAD CODE!
+                    //https://gist.github.com/natanfudge/6be2662ce8395bb14dc5c48157217e9e
+                    Vec3d pedestalPos = new Vec3d(x, y, z);
+                    Vec3d northPedestalPos = new Vec3d(x+3, y, z);
+                    int particleAmount = (int) (pedestalPos.distanceTo(northPedestalPos) * 4);
+
+                    for (int i = 0; i < particleAmount; i++) {
+                        Vec3d particlePos = pedestalPos.lerp(northPedestalPos, i / (float) particleAmount);
+
+                        world.addImportantParticle(new BlockStateParticleEffect(ParticleTypes.BLOCK, Blocks.RED_CONCRETE.getDefaultState()), particlePos.x, particlePos.y, particlePos.z, 0, 0, 0);
+                    }
 
                     ArcanistsParagon.LOGGER.info("Entity is (likely) spawned!");
                 }
